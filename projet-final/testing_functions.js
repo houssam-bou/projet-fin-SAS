4900a89b1f08530be5889793c766d0b2e8f45e05
houssam bounar <houssambounar67@gmail.com>
2026-09-08 22:14:53 +0100

    creating display function

--- projet-final/testing_functions.js
+++ projet-final/testing_functions.js
@@ -1,5 +1,3 @@
-let prompt = require("prompt-sync")();
-
 const trips = [
     {
         id: 1,
@@ -182,59 +180,7 @@ const trips = [
         availableSeats: 50
     }
 ];
-let choix;
-do {
-
-console.log("=================================");
-console.log("        RAILWAY MANAGER");
-console.log("=================================");
-
-
-console.log("1. Display trips");
-console.log("2. Buy a ticket");
-console.log("3. Display tickets");
-console.log("4. Cancel a ticket");
-console.log("5. Search for a ticket");
-console.log("6. Filter trips");
-console.log("7. Sort trips");
-console.log("0. Exit");
-
-    choix = Number(prompt("enter your choice = "));
-    switch (choix) {
-        case (1):
-            displayTrips();
-            break;
-        case (2):
-            
-            break;
-        case (3):
-            
-            break;
-        case (4):
-            
-            break;
-        case (5):
-            
-            break;
-        case (6):
-            
-            break;
-        case (0):
-            close();
-            break;
-        default:
-            console.log("error !! choisir une autre choix");
-            break;
-    }
-}
-while (choix != 0);
-
-function close() {
-    console.log("_____________________");
-    console.log("Vous avez quitté le programme.");
-    console.log("_____________________");
-    process.exit();
-}
+//<>=
 function displayTrips()
 {
     console.log("=== AVAILABLE TRIPS ===");
@@ -248,3 +194,4 @@ function displayTrips()
         console.log(`Available seats : ${trips[i].availableSeats} `);
     }
 }
+displayTrips();
